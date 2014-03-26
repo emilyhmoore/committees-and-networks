a74ba72c768e0d50672478c379e10f652541b719
emilyhmoore <emily.moore@wustl.edu>
2014-03-26 11:19:51 -0500

    Documentation, tests
    Added documentation and some tests/ plots to the script

--- score.generator.function.R
+++ score.generator.function.R
@@ -18,12 +18,13 @@ score.generator<-function(congress=1, abstain.agree=TRUE, senate){
   }
   
   house<-readKH(file) ##read in the file
-  n<-.025*house$n ##gets n for droping very lopsided votes
+  n<-.025*house$n ##gets n for dropping very lopsided votes
   house<-dropUnanimous(house,lop=n)
-  h.1<-house$votes ##First Congress Roll Call Votes
+  h.1<-house$votes ##Roll Call Votes
   h.1<-h.1[-1,] ##Dropping President
   h.1[h.1==0]<-NA ##Converting 0s to NAs
   h.1<-na.omit(h.1) ##Omitting 0s aka Not in Legislature for one or more votes
+  
   legnames<-rownames(h.1)
   agree.mat <- function(X, abstain=abstain.agree){
     X <- t(X) # put subjects on columns
@@ -44,36 +45,67 @@ score.generator<-function(congress=1, abstain.agree=TRUE, senate){
     data <- as.data.frame(A)
     return(data)
   }
+  ##create two of these since I manipulate them differently
   a<-agree.mat(h.1)
   b<-agree.mat(h.1)
+  
+  ##Set the legnames again
   legnames<-rownames(a)
+  ##Split names from extra junk
   legnames1<-unlist(strsplit(legnames,split=" \\("))
+  
+  ##Get rid of the extra junk 2
   odds<-seq(1,length(legnames1), by=2)
   just.names<-legnames1[odds]
+  
   n<-length(just.names)
+  
+  #Make enough names for this process to work
   enough.names<-rep(just.names,n)
   repped.names<-rep(just.names,c(rep(n,n)))
+  
+  #Set agreement to self to NA
   diag(a)<-NA
+  
+  ##Have a vector of these scores
   vector<-unlist(a)
   for(i in 1:length(vector)){
     names(vector)[i]<-paste(repped.names[i],enough.names[i],sep="_")
   }
-  vector<-na.omit(vector)
+  
+  ##Remove agreement with self
+  vector1<-na.omit(vector)
+  
+  ##Get rid of weird na.action attribute
+  attr(vector1, "na.action")<-NULL
+  
+  ##Get the adjacency graph for centrality
   G <- graph.adjacency(as.matrix(b),weighted=TRUE)
+  
   ## And then this will calculate and display the centrality scores
   centrality<-alpha.centrality(G)
-  #plot(density.default(x=alpha.centrality(G)))
-  return(list("agreement.scores"=vector, "centrality.scores"=centrality))
+
+  return(list("agreement.scores"=vector1, "centrality.scores"=centrality))
 }
 
 ##Try out the function
 trial2<-score.generator(congress=1,abstain.agree=FALSE, senate=TRUE)
 trial3<-score.generator(congress=1,abstain.agree=FALSE, senate=FALSE)
-trial4<-score.generator(congress=10, abstain.agree=TRUE, senate=TRUE)
-trial4<-score.generator(congress=10, abstain.agree=TRUE, senate=FALSE)
+trial4<-score.generator(congress=1, abstain.agree=TRUE, senate=TRUE)
+trial5<-score.generator(congress=1, abstain.agree=TRUE, senate=FALSE)
 
 agree<-trial3$agreement.scores
-str(agree)
+agree2<-trial5$agreement.scores
+
+plot(density(agree), col="red") ##any abstention auto treated as disagreement 
+lines(density(agree2), col="blue") ##two abstains treated as agreements
+
+agree3<-score.generator(20, FALSE, FALSE)$agreement.scores
+agree4<-score.generator(20, TRUE, FALSE)$agreement.scores
+
+plot(density(agree4), col="blue") ##two abstentions=agree
+lines(density(agree3), col="red") ##abstentions auto disagree
+
 #a.h1 <- as.data.frame(pairdata_full)
 #colnames(a.h1) <- c("rc_agree")
 #library(foreign)
